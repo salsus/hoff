67e00c2ab348736d99102505064c222847937de2
Sara <sara.hoffman@gmail.com>
2017-10-13 11:58:06 -0400

    Create javascript_tip.js

---
+++ en/javascript_tip.js
@@ -0,0 +1,11 @@
+/* 
+If you add script tags in Engaging Networks...
+
+Look for the EngagingNetworks object first and then run your code.
+Otherwise you can bonk your Admin UI really easily :(
+*/
+
+
+if (window.EngagingNetworks){
+		// Insert Your Code
+	}

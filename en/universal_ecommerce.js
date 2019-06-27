c3296ca1072da20a0a35434e12db095bef699a8d
Sara <sara.hoffman@gmail.com>
2019-06-27 11:06:03 -0400

    update category
    Fix formatting
    Add form id to the product name
    Add category for standard eCommerce tracking
    https://developers.google.com/analytics/devguides/collection/analyticsjs/ecommerce
    Add comments to addItem

--- en/universal_ecommerce.js
+++ en/universal_ecommerce.js
@@ -1,26 +1,40 @@
 // <![CDATA[
-    if (window.EngagingNetworks){
-// Standard Ecommerce Tracking for Engaging Networks
-    var initialAmount = '{receipt_data~amount}',
-        prettyAmount = initialAmount.replace(/\$/g, ''),
-        formName = jQuery("input[name='ea.form.id']").val();
-        
-    ga('require', 'ecommerce');
-    ga('ecommerce:addTransaction', {
-        'id': '{receipt_data~txId}',
-        'affiliation': 'Engaging Networks Donation Form',
-        'revenue': 'prettyAmount',
-        'shipping': '',
-        'tax': ''
-    });
-    ga('ecommerce:addItem', {
-        'id': '{receipt_data~txId}',
-        'name': formName,
-        'sku': '{receipt_data~campaignId}',
-        'category': '{receipt_data~type}',
-        'price': 'prettyAmount',  
-        'quantity': '1'
-    });
-    ga('ecommerce:send');
+if (window.EngagingNetworks) {
+  console.log(pageJson.pageName);
+  console.log(pageJson.pageNumber);
+  console.log(pageJson.trackingId);
+  console.log(pageJson.campaignId);
+
+  // Standard Ecommerce Tracking for Engaging Networks
+  var initialAmount = "{receipt_data~amount}",
+    prettyAmount = initialAmount.replace(/\$/g, ''),
+    donationFrequency = "{receipt_data~recurringFrequency}",
+    formName = pageJson.pageName + " - " + pageJson.campaignPageId;
+  console.log("frequency" + "{receipt_data~recurringFrequency}");
+
+  var productVariant;
+  if (donationFrequency) {
+    productVariant = donationFrequency;
+  } else {
+    productVariant = "OneTime";
+  }
+
+  ga("require", "ecommerce");
+  ga("ecommerce:addTransaction", {
+    id: "{receipt_data~txId}",
+    affiliation: "Engaging Networks Donation Form",
+    revenue: "prettyAmount",
+    shipping: "",
+    tax: ""
+  });
+  ga("ecommerce:addItem", {
+    id: "{receipt_data~txId}",
+    name: formName, // pageName + id: Renew Form - 1001
+    sku: pageJson.campaignPageId, // 1001
+    category: productVariant, // donation frequency, one-time or monthly,quarterly, etc
+    price: "prettyAmount",
+    quantity: "1"
+  });
+  ga("ecommerce:send");
 } // End window.EngagingNetworks
 // ]]>

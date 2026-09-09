90e83babf72e7cec3bc12f1572d130fceb487649
Hoff <sara.hoffman@gmail.com>
2026-09-09 13:55:44 -0400

    Create abbreviated_month.sql

---
+++ looker-studio-snippets/abbreviated_month.sql
@@ -0,0 +1,14 @@
+CASE Month
+  WHEN 1 THEN "Jan"
+  WHEN 2 THEN "Feb"
+  WHEN 3 THEN "Mar"
+  WHEN 4 THEN "Apr"
+  WHEN 5 THEN "May"
+  WHEN 6 THEN "Jun"
+  WHEN 7 THEN "Jul"
+  WHEN 8 THEN "Aug"
+  WHEN 9 THEN "Sep"
+  WHEN 10 THEN "Oct"
+  WHEN 11 THEN "Nov"
+  WHEN 12 THEN "Dec"
+END
